2b2d617446cfcad465fdc4647d123ec883730271
Michel Muniagiseny <mafri.michel@gmail.com>
2024-06-28 07:48:29 +0300

    Update schema

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,37 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface FaqFaqBlock extends Schema.Component {
+  collectionName: 'components_faq_faq_blocks';
+  info: {
+    displayName: 'FAQBlock';
+    icon: 'bulletList';
+  };
+  attributes: {
+    title: Attribute.String;
+    slug: Attribute.String;
+    faqItems: Attribute.Component<'faq.faq-item', true>;
+  };
+}
+
+export interface FaqFaqItem extends Schema.Component {
+  collectionName: 'components_faq_faq_items';
+  info: {
+    displayName: 'FAQItem';
+    icon: 'attachment';
+    description: '';
+  };
+  attributes: {
+    title: Attribute.String;
+    body: Attribute.RichText &
+      Attribute.CustomField<
+        'plugin::ckeditor5.CKEditor',
+        {
+          preset: 'toolbarBalloon';
+        }
+      >;
+  };
+}
+
 export interface MapSiteItem extends Schema.Component {
   collectionName: 'components_map_site_items';
   info: {
@@ -28,6 +60,8 @@ export interface MapSiteListItems extends Schema.Component {
 declare module '@strapi/types' {
   export module Shared {
     export interface Components {
+      'faq.faq-block': FaqFaqBlock;
+      'faq.faq-item': FaqFaqItem;
       'map-site.item': MapSiteItem;
       'map-site.list-items': MapSiteListItems;
     }
--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -362,6 +362,31 @@ export interface AdminTransferTokenPermission extends Schema.CollectionType {
   };
 }
 
+export interface ApiFaqFaq extends Schema.CollectionType {
+  collectionName: 'faqs';
+  info: {
+    singularName: 'faq';
+    pluralName: 'faqs';
+    displayName: 'FAQ';
+    description: '';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    title: Attribute.String;
+    url: Attribute.String;
+    faqBlocks: Attribute.Component<'faq.faq-block', true>;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<'api::faq.faq', 'oneToOne', 'admin::user'> &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<'api::faq.faq', 'oneToOne', 'admin::user'> &
+      Attribute.Private;
+  };
+}
+
 export interface ApiMapSiteMapSite extends Schema.SingleType {
   collectionName: 'map_sites';
   info: {
@@ -1040,6 +1065,7 @@ declare module '@strapi/types' {
       'admin::api-token-permission': AdminApiTokenPermission;
       'admin::transfer-token': AdminTransferToken;
       'admin::transfer-token-permission': AdminTransferTokenPermission;
+      'api::faq.faq': ApiFaqFaq;
       'api::map-site.map-site': ApiMapSiteMapSite;
       'plugin::upload.file': PluginUploadFile;
       'plugin::upload.folder': PluginUploadFolder;
